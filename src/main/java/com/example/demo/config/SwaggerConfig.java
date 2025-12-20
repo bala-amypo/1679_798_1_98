f7e1107e66d5f2d82c848d8838a9662ec079e3be
bala-amypo <bala@amypo.in>
2025-12-20 12:24:55 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 
 package com.example.demo.config;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,14 +7,11 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI api() {
         final String securitySchemeName = "JWT";
-
         return new OpenAPI()
                 .info(new Info()
                         .title("Micro-Learning Content Recommendation API")
