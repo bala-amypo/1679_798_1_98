dcf1250ed78076f12603b6c61df3689a283c951c
bala-amypo <bala@amypo.in>
2025-12-21 05:26:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,88 @@
+package com.example.demo.entity;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String fullName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+    @Column
+    private String preferredLearningStyle;
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getPreferredLearningStyle() {
+        return preferredLearningStyle;
+    }
+    public void setPreferredLearningStyle(String preferredLearningStyle) {
+        this.preferredLearningStyle = preferredLearningStyle;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public User() {
+    }
+    public User(Long id, String fullName, String email, String password, String role, String preferredLearningStyle,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.preferredLearningStyle = preferredLearningStyle;
+        this.createdAt = createdAt;
+    }
+    @PrePersist
+    protected void onCreate() {
+    this.createdAt = LocalDateTime.now();
+}
+}
