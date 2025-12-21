efd4b76cbb71ffab274ce28d7fd4e4a9f3dc4cb5
bala-amypo <bala@amypo.in>
2025-12-21 05:47:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecommendationRepository.java
+++ src/main/java/com/example/demo/repository/RecommendationRepository.java
@@ -1,13 +1,18 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.User;
+import com.example.demo.entity.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 
-    boolean existsByEmail(String email);
+    List<Recommendation> findByUserIdAndGeneratedAtBetween(
+            Long userId,
+            LocalDateTime start,
+            LocalDateTime end
+    );
 
-    Optional<User> findByEmail(String email);
+    List<Recommendation> findByUserIdOrderByGeneratedAtDesc(Long userId);
 }
