0df961a2f75e94a3762585b07da52753e74aa48a
bala-amypo <bala@amypo.in>
2025-12-22 05:24:53 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +11,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-            Server server = new Server();
+        Server server = new Server();
         server.setUrl("https://9088.408procr.amypo.ai/");
+        server.setDescription("College portal preview URL");
+
         return new OpenAPI()
+                .addServersItem(server)
                 .info(new Info()
                         .title("Demo Application API")
                         .version("1.0")
-                        .description("Swagger UI for Demo Application (No Security, No Auth)"));
+                        .description("Swagger UI for Demo Application"));
     }
 }
