771766ee8a4a702521d407ac853d5cc62155082a
bala-amypo <bala@amypo.in>
2025-12-22 05:09:49 +0000

    User commit

--- src/main/java/com/example/demo/dto/RecommendationRequest.java
+++ src/main/java/com/example/demo/dto/RecommendationRequest.java
@@ -1,17 +1,12 @@
 package com.example.demo.dto;
 import java.math.BigDecimal;
-
 public class RecommendationRequest {
-
     private BigDecimal confidenceScore;
-
     public RecommendationRequest() {
     }
-
     public BigDecimal getConfidenceScore() {
         return confidenceScore;
     }
-
     public void setConfidenceScore(BigDecimal confidenceScore) {
         this.confidenceScore = confidenceScore;
     }
