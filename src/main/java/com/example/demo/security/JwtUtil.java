2e7deae63f38fa362d19a38ea59ee0fa51240630
bala-amypo <bala@amypo.in>
2025-12-23 09:35:43 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,9 +1,7 @@
 package com.example.demo.security;
-
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
