bc4ad1e269533a7e7f71b0e3044e15f6acf3497b
bala-amypo <bala@amypo.in>
2025-12-26 06:18:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.Course;
 import com.example.demo.service.CourseService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
@@ -18,5 +20,20 @@ public class CourseController {
     public Course create(@PathVariable Long instructorId,
                          @RequestBody Course c) {
         return service.createCourse(c, instructorId);
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourse(@PathVariable Long id) {
+        return service.getCourseById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Course> getAllCourses() {
+        return service.getAllCourses();
+    }
+
+    @PutMapping("/{id}")
+    public Course updateCourse(@PathVariable Long id, @RequestBody Course c) {
+        return service.updateCourse(id, c);
     }
 }
