7772812eb8feea8985f5c44b09b64626d230c612
bala-amypo <bala@amypo.in>
2025-12-27 03:54:35 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.example.demo.config;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
